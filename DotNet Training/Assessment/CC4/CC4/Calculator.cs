152f353fbc7e34f34eba50a30ff48314fb3ee091
Sahana Navali <sahanan@infinite.com>
2024-07-10 11:11:39 +0530

    Assessment-4

---
+++ DotNet Training/Assessment/CC4/CC4/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC4
+{
+    class Calculator
+    {
+        delegate int Operation(int a, int b);
+
+        static void Main(string[] args)
+        {
+            Operation addOperation = PerformAddition;
+            Operation subtractOperation = PerformSubtraction;
+            Operation multiplyOperation = PerformMultiplication;
+
+            Console.Write("Enter the first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Result of Addition: {addOperation(firstNumber, secondNumber)}");
+            Console.WriteLine($"Result of Subtraction: {subtractOperation(firstNumber, secondNumber)}");
+            Console.WriteLine($"Result of Multiplication: {multiplyOperation(firstNumber, secondNumber)}");
+
+            Console.ReadKey();
+        }
+
+        static int PerformAddition(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int PerformSubtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int PerformMultiplication(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
+    
+
---
+++ DotNet Training/Assessment/CC4/CC4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace CC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\batch 2024 June\\DotNet Training\\Assessment\\CC4\\Sahana.txt";
+            string textToAppend = "Welcome to Infinite Computer Solutions";
+
+            AppendTextToFile(filePath, textToAppend);
+
+            Console.WriteLine("Text has Succesfully appended to the file.");
+            Console.ReadLine();
+        }
+
+        static void AppendTextToFile(string filePath, string text)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine(text);
+            }
+        }
+    }
+}
